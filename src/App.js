38bd2d27c07648cdc7877dee1ef54b05e1ac37dc
fuston05 <fuston05@yahoo.com>
2020-01-07 20:25:05 -0500

    forgot to commit more often. finished MVP, added basic styles to my components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+import Image from './components/Image/Image';
+import Info from './components/Info/Info';
 import "./App.css";
 
+
+
+
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
-    </div>
-  );
-}
+    
+    const [imgUrl, setImgUrl]= useState('');
+    const [hdUrl, setHdUrl]= useState('');
+    const [copy, setCopy]= useState('');
+    const [date, setDate]= useState('');
+    const [expl, setExpl]= useState('');
+    const [title, setTitle]= useState('');
+
+    useEffect(() => {
+        axios
+        .get('https://api.nasa.gov/planetary/apod?api_key=v8su2RncIsyRc8ZbQbgNobp0ndXwjixQPURTlhTc')
+        .then(res => {
+            console.log(res.data);
+            setImgUrl(res.data.url);
+            setHdUrl(res.data.hdurl);
+            setCopy(res.data.copy);
+            setDate(res.data.date);
+            setExpl(res.data.explanation);
+            setTitle(res.data.title);
+        })
+        .catch(err => {
+            console.log('Error: ', err);
+        }) 
+        
+    }, [])
+
+    return (
+        <div className="App">
+            <h1>Nasa: Astronomy Picture of the Day.</h1>
+            <div className= "mainContent">
+                <Image date= {date} imgUrl= {imgUrl} hdUrl={hdUrl} copy= {copy} />
+                <Info title= {title} expl= {expl} />
+            </div>
+            
+        </div>
+        
+
+    );
+    
+    
+
+}//end func
+
 
 export default App;
---
+++ src/components/Image/Image.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Image.css";
+
+
+function Image(props){
+
+
+    
+    return(
+        <div className= 'imageCont'>
+            <a title= 'Click to View HD Image' href= {props.hdUrl} target= '_blank'>
+            <span>Copyright &copy; {props.date}, {props.copy}</span>
+            <img alt= 'Nasa' src= {props.imgUrl} />
+            </a>
+        
+        </div>
+    );
+
+}//end func
+
+export default Image;
