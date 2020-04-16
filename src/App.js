6ffcb73119e6b1648c472fae9432e2ffaa520fc2
fuston05 <fuston05@yahoo.com>
2020-04-16 10:52:48 -0400

    adds dependencies and react app setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//styles
+import './sass/app.scss';
+
+//components
+
+function App() {
+  return (
+    <div className="App">
+      <h1>NASA Photo of the Day</h1>
+
+    </div>
+  );
+}
+
+export default App;
